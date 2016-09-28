6da34c44bd7e63e53f8e261048ba49790683239f
Ruan Aragão <ruanaragao2@gmail.com>
2016-09-28 15:16:26 -0300

    Add var path in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,12 @@ var concat   = require('gulp-concat');
 var cleanCss = require('gulp-clean-css');
 var notify   = require("gulp-notify");
 
-gulp.task('uglify-js', function () {
+var path = {
+    scripts: 'js/**/*',
+    styles: 'css/**/*'
+};
+
+gulp.task('uglify-js', function() {
   return gulp.src([
     'bower_components/jquery/dist/jquery.min.js',
     'bower_components/foundation-sites/dist/foundation.min.js',
@@ -17,7 +22,7 @@ gulp.task('uglify-js', function () {
   .pipe(notify('JS Minificado'));
 });
 
-gulp.task('minify-css', function () {
+gulp.task('minify-css', function() {
   return gulp.src([
     'bower_components/foundation-sites/dist/foundation.css',
     'bower_components/font-awesome/css/font-awesome.css',
@@ -27,14 +32,17 @@ gulp.task('minify-css', function () {
   .pipe(cleanCss())
   .pipe(concat('app.min.css'))
   .pipe(gulp.dest('./css/'))
-  .pipe(notify('CSS Minificado'));;
+  .pipe(notify('CSS Minificado'));
 });
 
-gulp.task('copy-fonts', function () {
+gulp.task('copy-fonts', function() {
   return gulp.src('bower_components/font-awesome/fonts/*')
   .pipe(gulp.dest('./fonts/'));
 });
 
-var watcher  = gulp.watch(['js/**/*', 'css/**/*'], ['uglify-js', 'minify-css']);
+gulp.task('watch', function() {
+    gulp.watch(path.scripts, ['uglify-js']);
+    gulp.watch(path.styles, ['minify-css']);
+});
 
-gulp.task('default', ['uglify-js', 'minify-css', 'copy-fonts']);
+gulp.task('default', ['watch', 'uglify-js', 'minify-css', 'copy-fonts']);
